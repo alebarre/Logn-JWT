dd5342f3cb0687e3926f7e137a373e80cc149ebf
Alexandre Barreto <plotartemail@gmail.com>
2026-08-09 22:17:35 -0300

    bugfix: Build da aplicação + insomnia e postman imports

--- src/main/java/com/br/login_jwt/LoginJwtApplication.java
+++ src/main/java/com/br/login_jwt/LoginJwtApplication.java
@@ -3,7 +3,6 @@ package com.br.login_jwt;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.MapPropertySource;
@@ -14,24 +13,24 @@ import java.util.Map;
 @SpringBootApplication
 public class LoginJwtApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication app = new SpringApplication(LoginJwtApplication.class);
+        SpringApplication app = new SpringApplication(LoginJwtApplication.class);
 
-		app.addInitializers((ApplicationContextInitializer<ConfigurableApplicationContext>) ctx -> {
-			Dotenv dotenv = Dotenv.configure()
-					.directory("./")
-					.ignoreIfMalformed()
-					.ignoreIfMissing()
-					.load();
+        app.addInitializers((ApplicationContextInitializer<ConfigurableApplicationContext>) ctx -> {
+            Dotenv dotenv = Dotenv.configure()
+                    .directory("./")
+                    .ignoreIfMalformed()
+                    .ignoreIfMissing()
+                    .load();
 
-			Map<String, Object> envMap = new HashMap<>();
-			dotenv.entries().forEach(e -> envMap.put(e.getKey(), e.getValue()));
+            Map<String, Object> envMap = new HashMap<>();
+            dotenv.entries().forEach(e -> envMap.put(e.getKey(), e.getValue()));
 
-			ctx.getEnvironment().getPropertySources()
-					.addFirst(new MapPropertySource("dotenvProperties", envMap));
-		});
+            ctx.getEnvironment().getPropertySources()
+                    .addFirst(new MapPropertySource("dotenvProperties", envMap));
+        });
 
-		app.run(args);
-	}
+        app.run(args);
+    }
 }
